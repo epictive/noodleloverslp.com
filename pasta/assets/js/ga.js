134e53e622a1d217b96c6b848e10ce2596fc96f1
Honggyu, Park <hg.park@epictive.net>
2022-10-19 20:21:06 +0900

    ga.js, mobile-detect.min.js 추가

--- pasta/assets/js/ga.js
+++ pasta/assets/js/ga.js
@@ -1,10 +1,13 @@
 $(function() {
+    // Mobile Detect
+    let md = new MobileDetect(window.navigator.userAgent);
+
     // Sample
     // data-gtag-action="<?=$device?>_CLICK" data-gtag-category="BANNER" data-gtag-label="생일기념 쿠폰"
     // GA Button Event
     $('.btn-ga').on('click', function() {
-        gtag('event', $(this).data('gtag-action') + '_' + $(this).data('gtag-category') + '_' + $(this).data('gtag-label'), {});
-        gtag('event', $(this).data('gtag-action'), {
+        gtag('event', (md.mobile() ? 'MO_' : 'PC_') + $(this).data('gtag-action') + '_' + $(this).data('gtag-category') + '_' + $(this).data('gtag-label'), {});
+        gtag('event', (md.mobile() ? 'MO_' : 'PC_') + $(this).data('gtag-action'), {
             'event_category' : $(this).data('gtag-category'),
             'event_label' : $(this).data('gtag-label')
         });
