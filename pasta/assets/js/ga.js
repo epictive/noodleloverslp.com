1e7d969dcfc768f17e061a539ffa111fc32543c2
Honggyu, Park <hg.park@epictive.net>
2022-10-21 17:19:42 +0900

    ga 적용

--- pasta/assets/js/ga.js
+++ pasta/assets/js/ga.js
@@ -3,7 +3,6 @@ $(function() {
     let md = new MobileDetect(window.navigator.userAgent);
 
     // Sample
-    // data-gtag-action="<?=$device?>_CLICK" data-gtag-category="BANNER" data-gtag-label="생일기념 쿠폰"
     // GA Button Event
     $('.btn-ga').on('click', function() {
         gtag('event', (md.mobile() ? 'MO_' : 'PC_') + $(this).data('gtag-action') + '_' + $(this).data('gtag-category') + '_' + $(this).data('gtag-label'), {});
